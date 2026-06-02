62a41df84fb601f202de1dc8485d574bd1c3aa96
Ana62-mp <ana.moposita84@gmail.com>
2026-06-01 22:36:48 -0500

    APMP: Añadir validacion de token y endpoint perfil

--- src/main/java/com/krakedev/jwt/controllers/AuthController.java
+++ src/main/java/com/krakedev/jwt/controllers/AuthController.java
@@ -3,10 +3,17 @@ package com.krakedev.jwt.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.krakedev.jwt.entidades.Usuario;
 import com.krakedev.jwt.services.UsuarioService;
+import com.krakedev.jwt.utils.JwtUtil;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -34,7 +41,9 @@ public class AuthController {
 			Usuario usuarioEncontrado = usuarioService.login(usuario.getUsername(), usuario.getPassword());
 
 			if (usuarioEncontrado != null) {
-				return ResponseEntity.ok(usuarioEncontrado);
+				String token = JwtUtil.generarToken(usuarioEncontrado.getUsername(), usuarioEncontrado.getRol());
+				
+				return ResponseEntity.ok("{\"token\":\""+ token+"\"}");
 			} else {
 				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
 			}
@@ -44,4 +53,34 @@ public class AuthController {
 					.body("Error al iniciar sesión: " + e.getMessage());
 		}
 	}
+	
+	
+	@GetMapping("/perfil")
+	public ResponseEntity<?> perfil(@RequestHeader("Authorization") String authHeader){
+		try {
+			if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token no enviado, formato incorrecto");
+			}
+			
+			String token = authHeader.replace("Bearer ", "");
+			
+			DecodedJWT jwt = JwtUtil.validarToken(token);
+			String username = jwt.getSubject();
+			String rol = jwt.getClaim("rol").asString();
+			
+			if("ADMIN".equals(rol)) {
+				return ResponseEntity.ok("Bienvenido adoptante del refugio " +username +". Tienes acceso.");
+			}else if("USER".equals(rol)) {
+				return ResponseEntity.ok("Bienvenido adoptante del refugio " +username +". Tienes acceso.");
+			}else {
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Rol no autorizado");
+			}
+			
+			
+		} catch(Exception e){
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token inválido o expirado.");
+		}
+	}
+	
+	
 }
